b80169071337fb9c9e4db9732251af9e3fa604d7
blinkant <58659872+blinkant@users.noreply.github.com>
2022-08-26 21:35:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,28 +1,28 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx235a08b0fb813434",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "e463963d6c2aef752a80c76b6e12676a",
     // 模板消息id
-    templateId: "",
+    templateId: "gZJjuNXElXSidJUglr-8bVpmqcjZ8ckyDcV6WkSpMeQ",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: ["ov7GK6G-8DfQMTHTxNbNwP4O6JuY"],
      
     // 信息配置
     // 所在省份
     province: "广东",
     // 所在城市
-    city: "惠州",
+    city: "广州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "老婆", "year": "1994", "date": "10-03", "r": 'new'},
+      {"name": "老公", "year": "1994", "date": "07-18", "r": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
+    loveDate: "2016-09-24",
     // 结婚纪念日
-    marryDate: "2020-01-04"
+    marryDate: "2023-08-08"
     }
 
 // {{date.DATA}}  
